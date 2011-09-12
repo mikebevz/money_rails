cae162b01f4515e3406d800ddb9da942d5d350f6
Nicholas Schultz-Møller <nicholas@subsis.com>
2011-09-12 11:29:01 +0200

    Use rspec.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,13 +1,14 @@
 require 'rubygems'
 require 'active_record'
 require 'active_support'
-require 'spec'
+require 'rspec'
 
 require 'money'
 require 'lib/money/rails'
 
 ActiveRecord::Base.establish_connection(
-  :adapter => 'sqlite3',
-  :database => ':memory:' )
+  :adapter  => 'sqlite3',
+  :database => ':memory:'
+)
 
 ActiveRecord::Migration.verbose = false
